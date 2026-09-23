a494795175c56952653025eabaea709ecf82e0fb
mehdiagrat <agratmehdi356@gmail.com>
2026-09-23 11:46:05 +0200

    Connexion reussi avec bs

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connecté !");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Erreur MongoDB :", error);
+  });
 
 app.get("/", (req, res) => {
-    res.send("Backend Express fonctionne !");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  res.send("Backend Express fonctionne !");
 });
